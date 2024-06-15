15e26049dcc90c44b51f7e2180e2c85dd8037cf6
Rajagopal143 <tech4fun18@gmail.com>
2024-06-15 18:08:38 +0530

    fileuploaded

--- src/Controllers/dropboxdb.controller.js
+++ src/Controllers/dropboxdb.controller.js
@@ -6,33 +6,21 @@ require("dotenv").config();
 const dbx = new Dropbox({
   accessToken:process.env.DROPBOX_SECRET_KEY,
 });
+
+
+
+
 const uploadFileToDropbox = async (req, res) => {
+  const data = fs.readFileSync("floorplan.json");
+  const jsonData = JSON.parse(data.toString());
+  
+  fs.writeFileSync("floorplan.json", JSON.stringify(jsonData));
+  console.log(jsonData);
   console.log(process.env.DROPBOX_SECRET_KEY);
-      if (req.method === "POST") {
-        const  fileContent  = req.body;
-
-        try {
-          // Step 1: Upload the file to Dropbox
-          const response = await dbx.filesUpload({
-            path: `/bp3dfiles/design.blueprint3d`,
-            contents: JSON.stringify(fileContent),
-            mode: "overwrite", // 'add' to create new file, 'overwrite' to replace existing file
-          });
-          const url = await dbx.filesGetTemporaryLink({
-            path: "/bp3dfiles/design.blueprint3d",
-          });
-
-          res
-            .status(200)
-            .json({ message: "File uploaded successfully", data: url });
-        } catch (error) {
-          console.error("Error uploading to Dropbox:", error);
-          res.status(500).json({ error: "Error uploading to Dropbox" });
-        }
-      } else {
-        res.status(405).json({ error: "Method not allowed" });
-      }
+    
   };
+
+
 
 
 // Example usage: Upload a sample file
